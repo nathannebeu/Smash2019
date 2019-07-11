8ab83cd030f6242d901ac2e48c70a8217dcd94da
Nathan Nebeu <52788712+nathannebeu@users.noreply.github.com>
2019-07-11 09:53:29 -0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,11 @@
+<script>
+<!--
+function confirmRefresh() {
+var okToRefresh = confirm("Do you really want to refresh the page?");
+if (okToRefresh)
+	{
+			setTimeout("location.reload(true);",1500);
+	}
+}
+// -->
+</script>
